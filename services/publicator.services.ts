56986273a61a92a3202e591a936651d461d58fd3
andritianaa <andritianasteve@gmail.com>
2023-10-31 18:23:31 +0300

    add publicator service

--- services/publicator.services.ts
+++ services/publicator.services.ts
@@ -1,1 +1,21 @@
-export const getByPublicator = 
+import { PublicatorModel } from "../models/publicator.model";
+import { ErrorHandler } from "../utils/error";
+
+export const getByPublicator = async (id: string): Promise<any> => {
+    const publicator = await PublicatorModel.findById(id)
+        .populate(
+            [
+                {
+                    path: 'publications',
+                    populate: [
+                        { path: 'commentaires', populate: 'user' },
+                        { path: 'reactions', populate: 'user' }
+                    ]
+                },
+                { path: 'userId', select: 'displayName email account_date image address country walletId coordonates' },
+                { path: 'pageId' }
+            ]
+        )
+    if (publicator) return publicator
+    else throw new ErrorHandler("Ce profile n'existe pas", 404, new Error)
+}
