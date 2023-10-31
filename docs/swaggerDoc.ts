db43051643438f2ea035cbe9b8975e59077e8cc9
andritianaa <andritianasteve@gmail.com>
2023-10-31 18:23:09 +0300

    import publicator to the docs

--- docs/swaggerDoc.ts
+++ docs/swaggerDoc.ts
@@ -3,9 +3,11 @@ import mainDoc from './mainDoc.json'
 import user from './user.json'
 import file from './file.json'
 import pages from './pages.json'
+import publicator from './publicator.json'
 import authentication from './authentication.json'
 const mergedDoc = {
     ...authentication,
+    ...publicator,
     ...file,
     ...user,
     ...pages
