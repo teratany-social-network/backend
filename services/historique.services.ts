3633337de688aaba3cbede3bd753229d19684195
andritianaa <andritianasteve@gmail.com>
2023-12-22 13:42:43 +0300

    BCK 1

--- services/historique.services.ts
+++ services/historique.services.ts
@@ -2,22 +2,25 @@ import { HistoriqueModel, IHistorique } from "../models/historique.model"
 import { ErrorHandler } from "../utils/error"
 
 export const addToHistory = async (historique: IHistorique) => {
-    await HistoriqueModel.findOneAndDelete({text: historique.text})
+    await HistoriqueModel.findOneAndDelete({ text: historique.text })
+    .catch((error) => {
+        throw new ErrorHandler('Erreur interne lors de l\'ajout d\'un nouveau élement dans l\'historique', 500, error)
+    })
     await new HistoriqueModel(historique).save()
         .catch((error) => {
-            throw new ErrorHandler('Erreur interne lors de l\'ajout de nouveau composant dans l\'historique', 500, error)
+            throw new ErrorHandler('Erreur interne lors de l\'ajout d\'un nouveau élement dans l\'historique', 500, error)
         })
 }
 
 export const removeFromHistoriques = async (id: string) => {
     await HistoriqueModel.findByIdAndDelete(id)
         .catch((error) => {
-            throw new ErrorHandler('Erreur interne lors de la suppression composant dans l\'historique', 500, error)
+            throw new ErrorHandler('Erreur interne lors de la suppression de l\'élement dans l\'historique', 500, error)
         })
 }
 
 export const getHistoriques = async (owner: string): Promise<IHistorique[]> => {
-    const historiques = await HistoriqueModel.find({ owner }).sort({date:-1})
+    const historiques = await HistoriqueModel.find({ owner }).sort({ date: -1 })
         .catch((error) => {
             throw new ErrorHandler('Impossible d\'avoir l\'historique', 500, error)
         })
